3da30f7e0f367a0ae76f42e8e36c37bc9df2073e
geospatialize <128581311+geospatialize@users.noreply.github.com>
2025-03-03 01:59:32 -0800

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: "/geospatialize.com/",  plugins: [react()],
+  base: "/geospatialize.com/", // Ensure this matches your repo name
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+      '@': path.resolve(__dirname, 'src')
+    }
   },
   build: {
-    sourcemap: true,
+    sourcemap: true
   },
   server: {
     fs: {
-      strict: false,
-    },
-  },
+      strict: false
+    }
+  }
 });
